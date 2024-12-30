2cc56e0279f319e11bb8cf617d5299b12fc696a9
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2024-12-30 19:55:08 -0300

    [BrickBreaker] Fix weird corner hits

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -13,12 +14,48 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log($"DBG: OnTriggerEnter2D other = {other}");
-
         switch (other.tag)
         {
             case "Block":
                 other.gameObject.GetComponent<Block>().GotHit();
+                Vector2 ballCenter = transform.position;
+                Vector2 blockCenter = other.transform.position;
+
+                Vector2 pointerVector = blockCenter - ballCenter;
+
+                float x = Math.Abs(pointerVector.x);
+                float y = Math.Abs(pointerVector.y);
+
+                if (Math.Abs(x - y) < 0.1)
+                {
+                    //quina
+                    bool vectorsHaveSameXSign = (rb.velocity.x > 0 && pointerVector.x > 0) || (rb.velocity.x < 0 && pointerVector.x < 0);
+                    bool vectorsHaveSameYSign = (rb.velocity.y > 0 && pointerVector.y > 0) || (rb.velocity.y < 0 && pointerVector.y < 0);
+                    if (vectorsHaveSameXSign && vectorsHaveSameYSign)
+                    {
+                        rb.velocity = new Vector2(rb.velocity.x * -1, rb.velocity.y * -1);
+                    }
+                    else
+                    {
+                        bool vectorsDifferOnX = (rb.velocity.x > 0 && pointerVector.x < 0) || (rb.velocity.x < 0 && pointerVector.x > 0);
+                        if (vectorsDifferOnX)
+                        {
+                            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * -1);
+                        }
+                        else
+                        {
+                            rb.velocity = new Vector2(rb.velocity.x * -1, rb.velocity.y);
+                        }
+                    }
+                }
+                else if (y > x)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * -1);
+                }
+                else if (x > y)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x * -1, rb.velocity.y);
+                }
                 break;
             case "TopBotWall":
                 rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * -1);
@@ -26,22 +63,6 @@ public class Ball : MonoBehaviour
             case "SideWall":
                 rb.velocity = new Vector2(rb.velocity.x * -1, rb.velocity.y);
                 break;
-                // default:
-                //     break;
         }
-
-        //checar se dá pra fazer um switch
-        // if (other.CompareTag("Block"))
-        // {
-        //     other.gameObject.GetComponent<Block>().GotHit();
-        // }
-        // else if (other.CompareTag("TopBotWall"))
-        // {
-        //     rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * -1);
-        // }
-        // else if (other.CompareTag("SideWall"))
-        // {
-        //     rb.velocity = new Vector2(rb.velocity.x * -1, rb.velocity.y);
-        // }
     }
 }
